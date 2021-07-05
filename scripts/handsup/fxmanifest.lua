c765b331ae2c113036b48eaf572260099c557960
cartWith5Wheels <82169925+cartWith5Wheels@users.noreply.github.com>
2021-07-05 15:51:28 +0200

    Create fxmanifest.lua

---
+++ scripts/handsup/fxmanifest.lua
@@ -0,0 +1,12 @@
+-- This file is critical for making the script function. If you change any file names the script will not work unless you correct it.
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author '@cartWith5Wheels at https://github.com/FiveM-Flux'
+description 'This is a simple and fully functional Lua script that lets players put their hands up with the press of a button. The default keybind is ALT.'
+version '1.0.0'
+
+-- What to run
+client_scripts {
+    'flux-handsup.lua'
+}
