db739bda9a4c80ccf9746b3af57055088bab7fc9
cartWith5Wheels <82169925+cartWith5Wheels@users.noreply.github.com>
2021-07-05 17:35:55 +0200

    Create fxmanifest.lua

---
+++ scripts/flux-id-display/fxmanifest.lua
@@ -0,0 +1,12 @@
+-- This file is critical for making the script function. If you change any file names the script will not work unless you correct it.
+fx_version 'cerulean'
+games { 'gta5' }
+
+author '@cartWith5Wheels at https://github.com/FiveM-Flux'
+description 'Lua script that lets players view their server ID above the map. No commands, no toggles, it is just there. I will NOT assist in remaking the script into a command, toggle or change the function as it is very possible to do for yourself.'
+version '1.0.0' -- Not important.
+
+-- If you want to run more files, you can add lines here separated by commas and line breaks.
+client_scripts {
+    'flux-id.lua'
+}
