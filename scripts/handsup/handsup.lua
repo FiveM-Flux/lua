f10798b38c73bd852a105381481a0187d35e7ac2
cartWith5Wheels <82169925+cartWith5Wheels@users.noreply.github.com>
2021-07-05 15:41:35 +0200

    Update handsup.lua

--- scripts/handsup/handsup.lua
+++ scripts/handsup/handsup.lua
@@ -1,4 +1,6 @@
--- Hold Left ALT to put your hands up.
+-- Press Left ALT to put your hands up.
+-- It is possible to walk, jog and run while holding your hands up.
+-- You only need to press the toggle - not hold.
 
 Citizen.CreateThread(function()
     local dict = "missminuteman_1ig_2"
@@ -10,7 +12,7 @@ Citizen.CreateThread(function()
     local handsup = false
 	while true do
 		Citizen.Wait(0)
-		if IsControlJustPressed(1, 19) then
+		if IsControlJustPressed(1, 19) then -- Want to change the keybind? Find the fitting one on https://docs.fivem.net/docs/game-references/controls/
             if not handsup then
                 TaskPlayAnim(GetPlayerPed(-1), dict, "handsup_enter", 8.0, 8.0, -1, 50, 0, false, false, false)
                 handsup = true
