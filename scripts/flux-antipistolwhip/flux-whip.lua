2391f409284f0185035b9c40237ddb9b81773eb4
cartWith5Wheels <82169925+cartWith5Wheels@users.noreply.github.com>
2021-07-05 16:43:49 +0200

    Create flux-whip.lua

---
+++ scripts/flux-antipistolwhip/flux-whip.lua
@@ -0,0 +1,12 @@
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+		local ped = PlayerPedId()
+            	if IsPedArmed(ped, 6) then
+	    	DisableControlAction(1, 140, true) -- This is the button R, it disables whipping using R
+            	DisableControlAction(1, 142, true) -- This is LMB, it disables whipping using the left mouse button
+        end
+    end
+end)
+ 
+-- To enable certain keybinds (such as using R to whip, but not LMB, simply toggle it by changing the "true" and "false" values.
