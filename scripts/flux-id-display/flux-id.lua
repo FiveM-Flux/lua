0b43fa743e4c48e6dd7c6b4d78c7f0a35dd62950
cartWith5Wheels <82169925+cartWith5Wheels@users.noreply.github.com>
2021-07-05 18:08:28 +0200

    Fixed me accidentally adding my old code.
    I'm dumb, it should be working now.

--- scripts/flux-id-display/flux-id.lua
+++ scripts/flux-id-display/flux-id.lua
@@ -1,9 +1,15 @@
-function FluxCoords(x,y ,width,height,scale, text, r,g,b,a, outline)
-    SetTextScale(scale, scale)
-	SetTextColour( 0,0,0, 255 )
-	SetTextOutline( 0,0,0, 255 )
-    SetTextFont(4)
-    SetTextEntry("string")
-    AddTextComponentString(text)
-    DrawText(x - width/2, y - height/2 + 0.005)
+Citizen.CreateThread(function()
+    while true do
+    FluxCoords(0.517, 1.270, 1.0,1.0,0.50, "~r~ID:~r~  ".. GetPlayerServerId(NetworkGetEntityOwner(GetPlayerPed(-1))) .. '', 255, 255, 255, 255)
+    Citizen.Wait(0)
+    end
+end)
+
+function FluxCoords(c1, c2, widthValue, heightValue, proportions, textDisplay, r,g,b,a)
+SetTextScale(proportions, proportions)
+SetTextEntry("string")
+AddTextComponentString(textDisplay)
+SetTextFont(4) -- There are 8 fonts available, ranging from 1-8
+SetTextColour(0, 0, 0, 255) -- This is calculated in RGBA, which means it supports RGB and Alpha colors.
+DrawText(c1 - widthValue/2, c2 - heightValue/2)
 end
