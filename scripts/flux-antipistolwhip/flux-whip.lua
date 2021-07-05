d3290c2bd8695981a2a2f36ab9be8f56655173b1
cartWith5Wheels <82169925+cartWith5Wheels@users.noreply.github.com>
2021-07-05 22:43:12 +0200

    Fix script not working after small revamp

--- scripts/flux-antipistolwhip/flux-whip.lua
+++ scripts/flux-antipistolwhip/flux-whip.lua
@@ -1,13 +1,15 @@
 Citizen.CreateThread(function()
-    while Citizen.Wait(30) do
-	local ped = PlayerPedId()
-        if IsPedArmed(ped, 6) then
-	    DisableControlAction(1, 140, true) -- This is the button R, it disables whipping using R
-	    -- DisableControlAction(1, 141, true) -- This is the button Q, it disables whipping using heavy weapons with Q
-            DisableControlAction(1, 142, true) -- This is LMB, it disables whipping using the left mouse button
+    while true do
+        Citizen.Wait(0)
+		
+		local ped = PlayerPedId()
+            	if IsPedArmed(ped, 6) then
+
+	    	DisableControlAction(1, 140, true) -- This is the button R, it disables whipping using R
+		   -- DisableControlAction(1, 141, true) -- This is the button Q, it disables whipping with heavy weapons using Q. This is commented out due to servers apparently not allowing this.
+            	DisableControlAction(1, 142, true) -- This is LMB, it disables whipping using the left mouse button
         end
     end
 end)
  
 -- To enable certain keybinds (such as using R to whip, but not LMB, simply toggle it by changing the "true" and "false" values.
--- Line 6 is commented due to heavy weapon meleeing not being available on every server. I don't exactly know what causes it to not work, but if it's possible on your server just uncomment that line.
