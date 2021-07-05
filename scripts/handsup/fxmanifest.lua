cd47d2e77053904089d51495eb22a06c0ec76527
cartWith5Wheels <82169925+cartWith5Wheels@users.noreply.github.com>
2021-07-05 15:52:43 +0200

    Update fxmanifest.lua

--- scripts/handsup/fxmanifest.lua
+++ scripts/handsup/fxmanifest.lua
@@ -6,7 +6,7 @@ author '@cartWith5Wheels at https://github.com/FiveM-Flux'
 description 'This is a simple and fully functional Lua script that lets players put their hands up with the press of a button. The default keybind is ALT.'
 version '1.0.0'
 
--- What to run
+-- If you want to run more files, you can add lines here separated by commas and line breaks.
 client_scripts {
     'flux-handsup.lua'
 }
