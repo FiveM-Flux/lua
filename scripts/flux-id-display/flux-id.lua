8bfcabd71b9b160cc2cc26fb7f61a0d6776b17a7
downloador <38085070+downloador@users.noreply.github.com>
2021-07-05 18:18:15 +0200

    Update flux-id.lua

--- scripts/flux-id-display/flux-id.lua
+++ scripts/flux-id-display/flux-id.lua
@@ -1,7 +1,7 @@
 function FluxCoords()
     SetTextScale(0.5, 0.5)
     SetTextEntry("string")
-    AddTextComponentString("~r~ID:~r~  ".. GetPlayerServerId(NetworkGetEntityOwner(GetPlayerPed(-1))) .. '')
+    AddTextComponentString("~g~Your ID:~w~  "..GetPlayerServerId(NetworkGetEntityOwner(GetPlayerPed(-1))).."")
     SetTextFont(4)
     SetTextColour(0, 0, 0, 255)
     DrawText(0.517 - 1.0/2, 1.270 - 1.0/2)
