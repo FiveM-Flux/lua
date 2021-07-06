e513f684d8dc6fe2fd01b8491c135207d0224550
downloador <38085070+downloador@users.noreply.github.com>
2021-07-06 07:16:22 +0200

    Update flux-whip.lua

--- scripts/flux-antipistolwhip/flux-whip.lua
+++ scripts/flux-antipistolwhip/flux-whip.lua
@@ -1,13 +1,11 @@
 Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(0)
-		
-		local ped = PlayerPedId()
-            	if IsPedArmed(ped, 6) then
-
-	    	DisableControlAction(1, 140, true) -- This is the button R, it disables whipping using R
-		   -- DisableControlAction(1, 141, true) -- This is the button Q, it disables whipping with heavy weapons using Q. This is commented out due to servers apparently not allowing this.
-            	DisableControlAction(1, 142, true) -- This is LMB, it disables whipping using the left mouse button
+    while Citizen.Wait(0) do
+	local ped = PlayerPedId()
+            if IsPedArmed(ped, 6) then
+ 
+	        DisableControlAction(1, 140, true) -- This is the button R, it disables whipping using R
+	        -- DisableControlAction(1, 141, true) -- This is the button Q, it disables whipping with heavy weapons using Q. This is commented out due to servers apparently not allowing this.
+                DisableControlAction(1, 142, true) -- This is LMB, it disables whipping using the left mouse button
         end
     end
 end)
