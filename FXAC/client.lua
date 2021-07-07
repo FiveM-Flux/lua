83eae04a4101edee2a29a272d448483372f8406c
downloador <38085070+downloador@users.noreply.github.com>
2021-07-07 14:41:23 +0200

    Add files via upload

---
+++ FXAC/client.lua
@@ -0,0 +1,20 @@
+Citizen.CreateThread(function()
+    -- Code usage from GitHub repository:
+    -- https://github.com/Blumlaut/anticheese-anticheat/blob/master/ac_c.lua
+
+    while Citizen.Wait(60000) do
+        local currentPlayer = PlayerPedId()
+        local currentHealth = GetEntityHealth(currentPlayer)
+
+        if currentHealth == 200 then
+            local setTo = currentHealth-math.random(1,10)
+            SetEntityHealth(currentPlayer, setTo)
+            Citizen.Wait(math.random(35,100))
+            if not IsPlayerDead(currentPlayer) then
+                if GetEntityHealth(currentPlayer) == 200 then
+                    print("Cheater")
+                end
+            end
+        end
+    end
+end)
