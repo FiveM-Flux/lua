a32f6215e2a7b357afb1502eade420b1a33549bc
cartWith5Wheels <82169925+cartWith5Wheels@users.noreply.github.com>
2021-07-08 20:31:34 +0200

    Change variable name

--- scripts/flux-id-display/flux-id.lua
+++ scripts/flux-id-display/flux-id.lua
@@ -1,9 +1,9 @@
-local temp_01 = GetPlayerServerId(NetworkGetEntityOwner(GetPlayerPed(-1)))
+local playerid = GetPlayerServerId(NetworkGetEntityOwner(GetPlayerPed(-1)))
 
 function FluxCoords()
     SetTextScale(0.5, 0.5) 
     SetTextEntry("string")
-    AddTextComponentString("~g~Your ID:~w~  "..temp_01) -- Colors can be found here: http://www.kronzky.info/fivemwiki/index.php/Text_Colors
+    AddTextComponentString("~g~Your ID:~w~  "..playerid) -- Colors can be found here: http://www.kronzky.info/fivemwiki/index.php/Text_Colors
     SetTextFont(4) -- There are 8 fonts ranging from 1-8. The fourth font is a bold arial-type font.
     SetTextColour(0, 0, 0, 255) -- RGBA
     DrawText(0.517 - 1.0/2, 1.270 - 1.0/2)
